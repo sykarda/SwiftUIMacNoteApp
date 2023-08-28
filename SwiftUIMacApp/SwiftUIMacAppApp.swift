03d16c6a58c1e42b6fb0f7cc1d754570c42856ab
sykarda <ardailgili@outlook.com>
2023-08-28 22:47:27 +0300

    Initial Commit

---
+++ SwiftUIMacApp/SwiftUIMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMacAppApp.swift
+//  SwiftUIMacApp
+//
+//  Created by Arda Ilgili on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
