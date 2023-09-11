0da05a275d47edf726e15c2bd25265080cb757d0
sykarda <ardailgili@outlook.com>
2023-09-11 12:07:09 +0300

    Application Menu deleted, now we are using popover to not show menu and the app in dock etc...

--- SwiftUIMacApp/SwiftUIMacAppApp.swift
+++ SwiftUIMacApp/SwiftUIMacAppApp.swift
@@ -9,38 +9,69 @@ import SwiftUI
 
 @main
 struct SwiftUIMacAppApp: App {
+
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        Settings {
-            EmptyView()
+        WindowGroup {
+            Text("Hello, world!")
         }
     }
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private var popover: NSPopover!
     static private(set) var instance: AppDelegate!
-    lazy var statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
-    let menu = ApplicationMenu()
+    private lazy var statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    private var contentView: ContentView!
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        menu.delegate = self
+        initContentView()
+        initPopover()
         AppDelegate.instance = self
+        initStatusBarItem()
+    }
+
+    private func initContentView() {
+        let contentView = ContentView()
+        self.contentView = contentView
+        self.contentView.delegate = self
+    }
+
+    private func initPopover() {
+        let popover = NSPopover()
+        popover.contentSize = CGSize(width: 400, height: 500)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: self.contentView)
+        self.popover = popover
+    }
+
+    private func initStatusBarItem() {
         statusBarItem.button?.title = getButtonTitle()
-        statusBarItem.menu = menu.createMenu()
+        statusBarItem.button?.action = #selector(togglePopover(_:))
     }
 
     private func getButtonTitle() -> String {
-        let notesCount = menu.getNumberOfNotes()
+        let notesCount = contentView.getNumberOfNotes()
         if notesCount > 0 {
             return String(notesCount)
         } else {
             return "🤗"
         }
     }
+
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
 }
 
-extension AppDelegate: ApplicatonMenuDelegate {
+extension AppDelegate: ContentViewDelegate {
     func notesCountChanged() {
         statusBarItem.button?.title = getButtonTitle()
     }
